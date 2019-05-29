576ad38064884aa550e8ddfc72591cc0ca2cb53e
Doctor-God <mycrazysideyt@hotmail.com>
2019-05-29 15:13:54 -0300

    Tá funcionando...?

--- cuda/final/final.cpp
+++ cuda/final/final.cpp
@@ -5,12 +5,22 @@
 #include <iostream>
 #include <fstream>
 #include <sstream>
+#include <cmath>
+#include <algorithm>
+#include <crypt.h>
+
+#define ABC_SIZE 64
+
+const char alfabeto[65] = "./0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
 using namespace std;
+
+string number2word(long num);
 
 int main(int argc, char ** argv){
 
     int *sndbuffer, recvbuffer;
-    int rank, size, i;
+    int rank, size, i, tag = 0;
 
     MPI_Init(&argc, &argv);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
@@ -23,13 +33,13 @@ int main(int argc, char ** argv){
    if(rank == 0){
         stringstream ss;
 
-        ifstream file("input");
+        ifstream file(argv[1]);
         
         string line;
         while (getline(file, line)){
             ss << line;
         }
-        cout << ss.str();
+        // cout << ss.str();
         file.close();
 
         cifrassize = ss.str().size() + 1;
@@ -47,20 +57,54 @@ int main(int argc, char ** argv){
 
 /* ----------------------------------- */
 
-    char alfabeto[65] = "./0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    string cifras_str = string(cifras);
 
-    int numero_cifras = (cifrassize - 1) / 13;
-    int cifra_atual = 0;
+    int numero_cifras = (cifrassize - 1) / 13; //Número de palavras codificas (cifras) a processar
+    int cifra_atual = 0; //Em qual cifra estamos no momento
     
-    int tamanho_cifra = 1;
-    int tamanho_maximo_cifra = 8;
+    int tamanho_cifra = 1; //Tamanho de senha testado no momento
+    int tamanho_maximo_cifra = 8; //Máximo tamanho possível da senha (resposta)
     
+    MPI_Status status;
+    int flag = false;
+    char* buffer = (char*) malloc(sizeof(char)*9);
 
-    while(numero_cifras--){
-
-        long numero_possibilidades = pow(64, tamanho_cifra);
-
+    long numero_possibilidades;
+    for(tamanho_cifra =1 ; tamanho_cifra <= tamanho_maximo_cifra; tamanho_cifra++){
+        numero_possibilidades += std::pow(ABC_SIZE, tamanho_cifra); 
     }
+
+    long rank_slice = numero_possibilidades/size; //quanto cada rank vai processar
+    while(numero_cifras--){
+        char* cifra = strdup(cifras_str.substr(cifra_atual*13, cifra_atual*13+13).c_str());
+
+        for(i = rank_slice*rank; i < rank_slice; i++){
+            char* palavra = strdup(number2word(i).c_str()); //Talvez seja bom colocar a função inline depois
+            if(strcmp(crypt(palavra, cifra), cifra) == 0){
+                cout << "Rank " << rank << " cifra:" << cifra_atual << " senha:" << string(palavra) << endl;
+                for(int j = 0; j < size; j++){
+                    if(j != rank)
+                        MPI_Send(palavra, strlen(palavra), MPI_CHAR, j, tag, MPI_COMM_WORLD);
+                }
+                // MPI_Bcast(palavra, strlen(palavra)+1, MPI_CHAR, rank, MPI_COMM_WORLD);
+                break;
+
+            }
+            // MPI_Bcast(buffer, 9, MPI_CHAR, MPI_ANY_SOURCE, MPI_COMM_WORLD);
+            // break;
+            MPI_Iprobe(MPI_ANY_SOURCE, tag, MPI_COMM_WORLD, &flag, &status);
+            if(flag){
+                // cout << "Rank estou aqui " << rank << endl; 
+                MPI_Recv(buffer, 9, MPI_CHAR, status.MPI_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
+                flag = false;
+                break;
+            }
+        }
+        // cout << "Rank " << rank << " cifra:" << cifra_atual << endl;
+        MPI_Barrier(MPI_COMM_WORLD);
+        cifra_atual++;
+    }
+
 
 
     MPI_Finalize();
@@ -69,3 +113,19 @@ int main(int argc, char ** argv){
     
 
 }
+
+string number2word(long num){
+    //14 (abc(4)abc(7))  = 4*64^1 + 7*64^0 = 263 
+    //263 = dividir sucessivamente, como de decimal para binário
+    stringstream palavra_ss;
+    ldiv_t temp;
+    temp.quot = num;
+    while(temp.quot != 0){
+        temp = ldiv(temp.quot, ABC_SIZE);
+        palavra_ss << alfabeto[temp.rem-1];
+    } 
+    string palavra = palavra_ss.str();
+    reverse(palavra.begin(), palavra.end());
+    return palavra;
+}
+
